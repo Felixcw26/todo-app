c3102676d613467df332e7c3f19af1b672b3a2d9
Felixcw26 <felixwinkler88@icloud.com>
2025-10-24 02:35:05 +0200

    Created better directory structure

---
+++ main.py
@@ -0,0 +1,5 @@
+from core.date_type import Date
+from core.todo_type import Priority, ToDo
+from core.todo_manager import ToDoManager
+from gui.dark_academia_theme import Palette, DarkAcademiaConsole
+from gui.console_ui import ConsoleUI
